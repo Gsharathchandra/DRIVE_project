0d0ead130e8c8c4656e22fc95f0fdeb9fcd3d7c4
Gsharathchandra <gumasthasharathchandra@gmail.com>
2025-05-30 10:39:57 +0530

    index

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const connectToDb = require('./config/db')
 const cookieParser = require('cookie-parser')
 connectToDb()
 const app = express();
+const indexRouter = require('./routes/index.routes')
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
@@ -15,7 +16,7 @@ app.set('view engine','ejs');
 app.get('/',(req,res)=>{
     res.render("index")
 })
-
+app.use('/',indexRouter)
 app.use('/user',userRouter)
 
 app.listen(3000,()=>{
