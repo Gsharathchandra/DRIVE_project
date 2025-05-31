2aa3591ca32b50116cc967a241e9ab117a17b7c5
Gsharathchandra <gumasthasharathchandra@gmail.com>
2025-05-31 19:59:09 +0530

    save

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'html');
+
 
 // Database connection
 connectToDb();
