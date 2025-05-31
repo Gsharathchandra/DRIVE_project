3ac6d2b15dc4107c8d3b94d7ce3ea412735c85ca
Gsharathchandra <gumasthasharathchandra@gmail.com>
2025-05-31 10:39:33 +0530

    save

--- app.js
+++ app.js
@@ -1,25 +1,53 @@
-const express = require('express');
-const userRouter = require('./routes/user.routes');
-// const {body,validationResult} =  require('express-validator');
-const dotenv = require('dotenv')
-dotenv.config()
-const connectToDb = require('./config/db')
-const cookieParser = require('cookie-parser')
-connectToDb()
-const app = express();
-const indexRouter = require('./routes/index.routes')
-app.use(cookieParser())
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.set('view engine','ejs');
+// const express = require('express');
+// const userRouter = require('./routes/user.routes');
+// // const {body,validationResult} =  require('express-validator');
+// const dotenv = require('dotenv')
+// dotenv.config()
+// const connectToDb = require('./config/db')
+// const cookieParser = require('cookie-parser')
+// connectToDb()
+// const app = express();
+// const indexRouter = require('./routes/index.routes')
+// app.use(cookieParser())
+// app.use(express.json())
+// app.use(express.urlencoded({extended:true}))
+// app.set('view engine','ejs');
 
-app.get('/',(req,res)=>{
-    res.render("index")
-})
-app.use('/',indexRouter)
-app.use('/user',userRouter)
+// app.get('/',(req,res)=>{
+//     res.render("index")
+// })
+// app.use('/',indexRouter)
+// app.use('/user',userRouter)
 
-app.listen(3000,()=>{
-    console.log("running baby");
+// app.listen(3000,()=>{
+//     console.log("running baby");
     
-})
+// })
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const connectToDb = require('./db/connect');
+const userRouter = require('./routes/user.routes');
+const fileRouter = require('./routes/file.routes');
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Connect to DB
+connectToDb();
+
+// Routes
+app.use('/user', userRouter);
+app.use('/files', fileRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ models/file.model.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const { GridFSBucket } = require('mongodb');
+
+// File schema for metadata
+const fileSchema = new mongoose.Schema({
+  filename: {
+    type: String,
+    required: true
+  },
+  contentType: {
+    type: String,
+    required: true
+  },
+  length: {
+    type: Number,
+    required: true
+  },
+  uploadDate: {
+    type: Date,
+    default: Date.now
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true
+  },
+  parentFolder: {
+    type: String,
+    default: 'root'
+  },
+  isPublic: {
+    type: Boolean,
+    default: false
+  }
+});
+
+// Create GridFS bucket
+let gfs;
+mongoose.connection.once('open', () => {
+  gfs = new GridFSBucket(mongoose.connection.db, {
+    bucketName: 'uploads'
+  });
+});
+
+const File = mongoose.model('File', fileSchema);
+
+module.exports = { File, gfs };
