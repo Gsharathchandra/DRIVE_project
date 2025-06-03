45224edb2846bf401dae8b593f17b357cf136545
Gsharathchandra <gumasthasharathchandra@gmail.com>
2025-06-03 10:12:28 +0530

    save completed

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,25 +1,19 @@
-const jwt = require('jsonwebtoken');
-
-   const authMiddleware = (req, res, next) => {
-     try {
-       const token = req.cookies.token;
-       
-       if (!token) {
-         return res.status(401).json({
-           success: false,
-           message: 'Please log in to access this resource',
-         });
-       }
-
-       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-       req.user = decoded;
-       next();
-     } catch (error) {
-       res.status(400).json({
-         success: false,
-         message: 'Invalid or expired token',
-       });
-     }
-   };
-
-   module.exports = authMiddleware;
+const authMiddleware = (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: 'Please log in to access this resource',
+      });
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: 'Invalid or expired token',
+    });
+  }
+};
